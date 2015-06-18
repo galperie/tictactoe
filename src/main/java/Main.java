d8ad39dd55dd9a11c7f6482a22d0ecab828407ab
Ellen <galperie@lafayette.edu>
2015-06-18 13:22:35 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+package main.java;
+
+/**
+ * Created by egalperi on 6/18/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ test/java/MainTest.java
@@ -0,0 +1,7 @@
+package java;
+
+/**
+ * Created by egalperi on 6/18/15.
+ */
+public class MainTest {
+}
